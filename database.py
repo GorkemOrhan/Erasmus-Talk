e61b5e74821374ce9bd5d2b1ba9adc8b76bdcb8e
GorkemOrhan <gorkemorhan10@gmail.com>
2024-06-28 15:07:41 +0300

    db connection small changes

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine,text
+import os
 
-engine = create_engine('REDACTED')
+db_connection_string = os.environ["DB_CONNECTION_STR"]
+db_connection_string = db_connection_string.strip('"').strip("'")
 
-with engine.connect() as conn:
-    result = conn.execute(text("select * from students"))
+engine = create_engine(db_connection_string)
 
-    result_dicts = []
-    for row in result.all():
-        result_dicts.append(dict(row._mapping))
-    print(result_dicts)
+def load_students_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from students"))
+        students = []
+        for row in result.all():
+            students.append(dict(row._mapping))
+        return students
--- start.py
+++ start.py
@@ -1,38 +1,17 @@
 from flask import Flask, render_template,jsonify
+from database import load_students_from_db
 
 app = Flask(__name__)
 
-STUDENTS = [
-    {
-        "id": 1,
-        "name": "Görkem",
-        "surname": "Orhan",
-        "department":"Computer Engineering",
-        "going_to":"Bologna Universitty"
-    },
-    {
-        "id": 2,
-        "name": "John",
-        "surname": "Connor",
-        "department":"Industrial Engineering",
-        "going_to":"Frei Universitat"
-    },
-    {
-        "id": 3,
-        "name": "Marcus",
-        "surname": "Pietro",
-        "department":"Electrical-Electronics Engineer",
-        "going_to":"Antalya Bilim University"
-    }
-]
-
 @app.route("/")
 def hello_world():
-    return render_template("home.html",students=STUDENTS)
+    students = load_students_from_db()
+    return render_template("home.html",students=students)
 
 @app.route("/api/students")
 def list_students():
-    return jsonify(STUDENTS)
+    students = load_students_from_db()
+    return jsonify(students)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
