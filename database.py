619b9c08d460ba366c675b8875324fa5d2a43b73
GorkemOrhan <gorkemorhan10@gmail.com>
2024-06-29 14:44:31 +0300

    dynamic pages for each student

--- database.py
+++ database.py
@@ -8,8 +8,18 @@ engine = create_engine(db_connection_string)
 
 def load_students_from_db():
     with engine.connect() as conn:
-        result = conn.execute(text("select * from students"))
+        result = conn.execute(text("SELECT * FROM students"))
         students = []
         for row in result.all():
             students.append(dict(row._mapping))
         return students
+    
+def load_student_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM students WHERE id = :val"), {"val": id})
+        row = result.fetchone()
+        if row is None:
+            return None
+        else:
+            return dict(row._mapping)
+        
--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,jsonify
-from database import load_students_from_db
+from database import load_students_from_db, load_student_from_db
 
 app = Flask(__name__)
 
@@ -12,6 +12,15 @@ def hello_world():
 def list_students():
     students = load_students_from_db()
     return jsonify(students)
+
+@app.route("/student/<id>")
+def show_student(id):
+    student = load_student_from_db(id)
+
+    if not student:
+        return "Not Found",404
+    
+    return render_template("studentpage.html",student=student)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
